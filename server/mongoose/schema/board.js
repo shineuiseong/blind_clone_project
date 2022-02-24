936f1d27f1b348ac7c71e01f1eb3b53c064c71dc
Shineuiseong <crossstory12@gmail.com>
2022-02-24 19:33:01 +0900

    Schema implementation complete

---
+++ server/mongoose/schema/board.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Board = new Schema({
+  title: { type: String, required: true, unique: true },
+  url: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now, required: true },
+})
+
+module.exports = Board
--- server/mongoose/schema/comment.js
+++ server/mongoose/schema/comment.js
@@ -2,18 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Comment = new Schema({
-  title: { type: String, required: true, unique: true },
+  article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
   content: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, required: true },
 
   // 동적으로 변동될 수 있는 데이터
-  viewCount: { type: Number, default: 0 },
   thumbupCount: { type: Number, default: 0 },
-  commentCount: { type: Number, default: 0 },
 
   //(옵션): 사용자가 게시글에 추가할 수 있는 데이터
   articleImgAddress: { type: String, default: null },
-  mention: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 })
 
 module.exports = Comment
