de8861c0256fb27b3cd1873c62a1db0ff9db9e3a
Shineuiseong <crossstory12@gmail.com>
2022-02-24 19:18:39 +0900

    mongoose 설정 및 스키마 작성

---
+++ server/mongoose/schema/company.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Company = new Schema({
+  title: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now, required: true },
+  reviewscore: { type: String, default: 3 },
+  imgAddress: { type: String, default: null },
+  realtimeScore: { type: Number, default: 0 }, // 가변적이라 따로 뺴두는게 좋지만 시간상 진행 리팩토링때 다시진행할예정
+})
+
+module.exports = Company
