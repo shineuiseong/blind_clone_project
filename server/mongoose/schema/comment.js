fab0bf0f298b1d8d1c24bb27fb675d2f348fc3a7
Shineuiseong <crossstory12@gmail.com>
2022-02-24 21:28:27 +0900

    Schema 통합

--- server/mongoose/schema/comment.js
+++ server/mongoose/schema/comment.js
@@ -12,9 +12,7 @@ const Comment = new Schema({
   thumbupCount: { type: Number, default: 0 },
 
   //(옵션): 사용자가 게시글에 추가할 수 있는 데이터
-  articleImgAddress: { type: String, default: null },
-
-  replies: [],
+  articleImgAddress: { type: String },
 })
 
 module.exports = Comment
