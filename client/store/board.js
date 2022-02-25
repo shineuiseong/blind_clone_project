8300d8352789bdd818225e03becbd570518efe89
Shineuiseong <crossstory12@gmail.com>
2022-02-25 21:41:59 +0900

    client store 내용추가

---
+++ client/store/board.js
@@ -0,0 +1,8 @@
+// 게시판 상태
+export const state = () => ({});
+
+// 상태변화
+export const mutations = {};
+
+// 상태와 관련된 행동
+export const actions = {};
---
+++ client/store/modal.js
@@ -0,0 +1,23 @@
+// 모달 상태
+export const state = {
+  login: { show: false, directLogin: false },
+};
+
+// 상태변화
+export const mutations = {
+  SET_LOGIN_MODAL_OPEN({ state }) {
+    state.login.show = true;
+  },
+  SET_LOGIN_MODAL_DIRECT_LOGIN({ state }) {
+    state.login.directLogin = true;
+  },
+  SET_LOGIN_MODAL_CLOSE({ state }) {
+    state.login = {
+      show: false,
+      directLogin: false,
+    };
+  },
+};
+
+// 상태와 관련된 행동
+export const actions = {};
