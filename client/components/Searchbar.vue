1ee7dd55fe38a16a946f1f16c8a77ddb2a7efc07
Shineuiseong <crossstory12@gmail.com>
2022-02-28 18:18:05 +0900

    Update Searchbar

--- client/components/Searchbar.vue
+++ client/components/Searchbar.vue
@@ -1,13 +1,41 @@
 <template lang="">
-    <div>
-        
+    <div class ="wrap-searchbar">
+        <SearchIcon class="search-icon"/>
+        <input v-model="query" type="text"  placeholder="관심있는 내용을 검색해보세요"/>
     </div>
 </template>
 <script>
+import { SearchIcon } from 'vue-feather-icons'
 export default {
+    data() {
+        return {
+            query: null,
+        }
+    },
+    components: {
+        SearchIcon,
+    },
     
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+.wrap-searchbar {
+  width: 100%;
+  margin: 40px 0 20px;
+  & > .search-icon {
+    position: absolute;
+    margin-left: 20px;
+    margin-top: 18px;
+  }
+  & > input {
+    display: block;
+    width: inherit;
+    height: 60px;
+    padding: 0 10px 0 62px;
+    border: 2px solid #222;
+    font-size: 18px;
+    border-radius: 30px;
+    box-sizing: border-box;
+  }
+}
 </style>
